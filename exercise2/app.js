8a535dfdaffee8da7a5227f160b73a7c2e7368b6
Adam Mackintosh <agm1984@gmail.com>
2018-05-26 13:53:31 -0700

    completed exercise 2

--- exercise2/app.js
+++ exercise2/app.js
@@ -1,6 +1,14 @@
 new Vue({
-        el: '#exercise',
-        data: {
-            value: ''
+    el: '#exercise',
+    data: {
+        value: ''
+    },
+    methods: {
+        showAlert: function() {
+            alert('wow')
+        },
+        updateValue: function(event) {
+            this.value = event.target.value
         }
-    });
+    },
+})
